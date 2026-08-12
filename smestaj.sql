c5931a5faa15f87db10fc222f0c2a48234f9b015
Lenka <lselakov2001@gmail.com>
2026-08-12 16:50:57 +0200

    Tačka-zarez kod USE

--- smestaj.sql
+++ smestaj.sql
@@ -8,7 +8,7 @@
 -- PHP Version: 8.2.12
 
 CREATE DATABASE IF NOT EXISTS `smestaj`;
-USE `smestaj`
+USE `smestaj`;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
